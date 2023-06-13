63474c1e925c42f2f98c18de3f7ae34bc67685ab
Sam Raj <rajsam003@gmail.com>
2023-06-13 15:16:59 +0200

    ok

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+import React from "react"
+
+const Logo = () => {
+  const router = useRouter
+
+  return (
+    <Image
+      src="/images/logo.png"
+      alt="Logo"
+      className="hidden md:block cursor-pointer"
+      height="100"
+      width="100"
+    />
+  )
+}
+
+export default Logo
